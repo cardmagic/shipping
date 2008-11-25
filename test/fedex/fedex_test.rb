74ad6a39fc2f12e5dfb969ad3c86473a56ccabac
Jimmy Baker <jimmybaker@jimmy.tol.lan>
2008-11-25 16:05:02 -0600

    You can now call @fedex.available_services which returns an array of fedex shipping objects. These objects contain the price and service among other things.

--- test/fedex/fedex_test.rb
+++ test/fedex/fedex_test.rb
@@ -64,6 +64,23 @@ class FedExTest < Test::Unit::TestCase
 		assert @ship.void(@label.tracking_number)
 	end
 	
+	def test_available_services
+	  @ship.name = "Package Receiver"
+		@ship.phone = "555-555-5555"
+		@ship.email = "lucas@rufy.com"
+		@ship.address = "Some address"
+		@ship.city = "Portland"
+	
+		@ship.sender_name = "Package Sender"
+		@ship.sender_phone = "333-333-3333"
+		@ship.sender_email = "john@doe.com"
+		@ship.sender_address = "Ok old address"
+		@ship.sender_city = "New York"
+		
+		assert_nothing_raised { @available_services = @ship.available_services }
+		assert_not_nil @available_services
+	end
+	
 	def test_void_fail
 		# made up tracking number
 		assert_raise(Shipping::ShippingError) { @ship.void('470012923511666') }
